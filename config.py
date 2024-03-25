36effcdcabc8c155bb4e6b75412904654d704e98
Exorcist V <dungnguyenx2077@gmail.com>
2024-03-25 18:28:48 +0700

    config

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # Connect to database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:NuHFMYkYalcNPYjebMtqLERYAORZCXry@monorail.proxy.rlwy.net:38821/railway'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:NuHFMYkYalcNPYjebMtqLERYAORZCXry@monorail.proxy.rlwy.net:38821/railway'
 app.config['SQLAlCHEMY_TRACK_MODIFICATIONS'] = True
 
 # Creating data base
